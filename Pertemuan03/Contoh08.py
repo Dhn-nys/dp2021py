0273e0c5a1dd91decf82d68eb4115641319e9293
デニス <dhenys.1002@gmail.com>
2021-10-31 20:32:42 +0700

    Upload Contoh06.py, Contoh07.py, and Contoh08.py
    [ Date: Sun 31 Oct 2021 ]
    [ Time: 20:32 PM]
    Update ^.^ 3 files

---
+++ Pertemuan03/Contoh08.py
@@ -0,0 +1,9 @@
+# Menggunakan posisi default
+default_order = "{}, {} dan {}".format("Budi","Galih","Ratna")
+print('\n--- Urutan default ---')
+print(default_order)
+
+# Menggunakan posisi default
+positional_order = "{1}, {0} dan {2}".format("Budi","Galih","Ratna")
+print('\n--- Urutan default ---')
+print(positional_order)
